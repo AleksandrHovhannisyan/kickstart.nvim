4c3d508f1209cd979c11ba72f5fb00a1844be246
AleksandrHovhannisyan <ahovhannisyan@esri.com>
2024-10-22 10:26:52 -0500

    Update tsserver lsp

--- init.lua
+++ init.lua
@@ -581,7 +581,7 @@ require('lazy').setup({
       local servers = {
         -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
         eslint = {},
-        tsserver = {},
+        ts_ls = {},
         clangd = {},
         rust_analyzer = {},
         dockerls = {},
